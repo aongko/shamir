6154433a4ef4baba21fb65534092e6e14ee1d1ac
Teddy Budiono Hermawan <ted.db.mon@gmail.com>
2013-05-30 13:36:58 +0700

    Lupa update apa, kayaknya nambahin breadcrumbs
    modified:   protected/modules/Learn/views/default/classDetail.php

--- protected/modules/Learn/views/default/classDetail.php
+++ protected/modules/Learn/views/default/classDetail.php
@@ -1,4 +1,11 @@
 <?php
+$this->pageTitle=Yii::app()->name . ' - ' . $this->module->id;
+$this->breadcrumbs=array(
+	$this->module->id, $model->class_name
+);
+?>
+
+<?php
 echo 'class_name : ' . $model->class_name . '</br>';
 echo 'max_capacity : ' . $model->max_capacity . '</br>';
 echo 'description : ' . $model->description . '</br>';
