2588a90f855919038f52ee9a82de0ac078dd9cfe
Andrew Ongko <Andrew.Ongko@gmail.com>
2013-06-09 19:29:31 +0700

    contact us

--- protected/config/main.php
+++ protected/config/main.php
@@ -105,6 +105,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'andrew.ongko@gmail.com',
 	),
 );
--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -65,11 +65,13 @@ If you have business inquiries or other questions, please fill out the following
 	<div class="row">
 		<?php echo $form->labelEx($model,'verifyCode'); ?>
 		<div>
-		<?php $this->widget('CCaptcha'); ?>
-		<?php echo $form->textField($model,'verifyCode'); ?>
+			<?php $this->widget('CCaptcha'); ?>
+			<br/>
+			<?php echo $form->textField($model,'verifyCode'); ?>
 		</div>
 		<div class="hint">Please enter the letters as they are shown in the image above.
-		<br/>Letters are not case-sensitive.</div>
+			<br/>Letters are not case-sensitive.
+		</div>
 		<?php echo $form->error($model,'verifyCode'); ?>
 	</div>
 	<?php endif; ?>
