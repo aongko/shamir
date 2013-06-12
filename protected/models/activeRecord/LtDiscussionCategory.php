d5982c78dcc8626dfa4e728af17c40999f7a509f
Teddy Budiono Hermawan <ted.db.mon@gmail.com>
2013-06-12 21:53:03 +0700

    no msg lagi

--- protected/models/activeRecord/LtDiscussionCategory.php
+++ protected/models/activeRecord/LtDiscussionCategory.php
@@ -6,12 +6,14 @@
  * The followings are the available columns in table 'lt_discussion_category':
  * @property integer $discussion_category_id
  * @property string $discussion_category_name
+ * @property integer $class_id
  * @property integer $parent_id
  * @property string $user_input
  * @property string $input_date
  * @property string $status_record
  *
  * The followings are the available model relations:
+ * @property MsClass $class
  * @property LtDiscussionCategory $parent
  * @property LtDiscussionCategory[] $ltDiscussionCategories
  * @property TrDiscussion[] $trDiscussions
@@ -45,13 +47,13 @@ class LtDiscussionCategory extends CActiveRecord
 		// will receive user inputs.
 		return array(
 			array('discussion_category_name', 'required'),
-			array('parent_id', 'numerical', 'integerOnly'=>true),
+			array('class_id, parent_id', 'numerical', 'integerOnly'=>true),
 			array('discussion_category_name, user_input', 'length', 'max'=>50),
 			array('status_record', 'length', 'max'=>1),
 			array('input_date', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('discussion_category_id, discussion_category_name, parent_id, user_input, input_date, status_record', 'safe', 'on'=>'search'),
+			array('discussion_category_id, discussion_category_name, class_id, parent_id, user_input, input_date, status_record', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -63,6 +65,7 @@ class LtDiscussionCategory extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+			'class' => array(self::BELONGS_TO, 'MsClass', 'class_id'),
 			'parent' => array(self::BELONGS_TO, 'LtDiscussionCategory', 'parent_id'),
 			'ltDiscussionCategories' => array(self::HAS_MANY, 'LtDiscussionCategory', 'parent_id'),
 			'trDiscussions' => array(self::HAS_MANY, 'TrDiscussion', 'discussion_category_id'),
@@ -77,6 +80,7 @@ class LtDiscussionCategory extends CActiveRecord
 		return array(
 			'discussion_category_id' => 'Discussion Category',
 			'discussion_category_name' => 'Discussion Category Name',
+			'class_id' => 'Class',
 			'parent_id' => 'Parent',
 			'user_input' => 'User Input',
 			'input_date' => 'Input Date',
@@ -97,6 +101,7 @@ class LtDiscussionCategory extends CActiveRecord
 
 		$criteria->compare('discussion_category_id',$this->discussion_category_id);
 		$criteria->compare('discussion_category_name',$this->discussion_category_name,true);
+		$criteria->compare('class_id',$this->class_id);
 		$criteria->compare('parent_id',$this->parent_id);
 		$criteria->compare('user_input',$this->user_input,true);
 		$criteria->compare('input_date',$this->input_date,true);
